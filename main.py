3515a0897782c5dd2e8144d1fa7611982723514a
mradul2 <theroyalpekka@gmail.com>
2021-11-29 16:01:53 +0530

    Combining slow and fast pathways

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from utils.def_config import assert_and_infer_cfg
 from utils.parser import load_config, parse_args
 import torch
+import torch.nn as nn
 
 # Trained SlowFast R101 on AVA 2.2, pre-trained on Kinetics 600 (mAP: 29.4)
 path = '/content/drive/MyDrive/SLOWFAST_64x2_R101_50_50A.pkl'
@@ -27,7 +28,7 @@ slow_pathway = torch.index_select(
             frames,
             2,
             torch.linspace(
-                0, frames.shape[2] - 1, frames.shape[2] // 16
+                0, frames.shape[2] - 1, frames.shape[2] // 8
             ).long(),
         ).to('cuda')
 
@@ -36,3 +37,11 @@ frame_list = [slow_pathway, fast_pathway]
 rand_input = frame_list
 rand_output = mymodel(rand_input)
 print(rand_output[0].shape, rand_output[1].shape)
+
+feats = rand_output
+# temporal average pooling
+h, w = feats[0].shape[3:]
+# requires all features have the same spatial dimensions
+feats = [nn.AdaptiveAvgPool3d((1, h, w))(f).view(-1, f.shape[1], h, w) for f in feats]
+feats = torch.cat(feats, dim=1)
+print(feats.shape)

aaf09d8fedb55e3981219ae7c0f5da7b6810715a
mradul2 <theroyalpekka@gmail.com>
2021-12-12 20:29:36 +0530

    Update

--- datasets/ava_helper.py
+++ datasets/ava_helper.py
@@ -1,304 +1,236 @@
-# Copyright (c) Facebook, Inc. and its affiliates.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-##############################################################################
-#
-# Based on:
-# --------------------------------------------------------
-# ActivityNet
-# Copyright (c) 2015 ActivityNet
-# Licensed under The MIT License
-# [see https://github.com/activitynet/ActivityNet/blob/master/LICENSE for details]
-# --------------------------------------------------------
+#!/usr/bin/env python3
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
 
-"""Helper functions for AVA evaluation."""
-
-from __future__ import (
-    absolute_import,
-    division,
-    print_function,
-    unicode_literals,
-)
-import csv
-import numpy as np
-import pprint
-import time
+import os
 from collections import defaultdict
-
-import utils.distributed as du
-from utils.ava_evaluation import (
-    object_detection_evaluation,
-    standard_fields,
-)
 
 from iopath.common.file_io import PathManagerFactory
 pathmgr = PathManagerFactory.get(key="pyslowfast")
 
 
-
-def make_image_key(video_id, timestamp):
-    """Returns a unique identifier for a video id & timestamp."""
-    return "%s,%04d" % (video_id, int(float(timestamp)))
+FPS = 30
+AVA_VALID_FRAMES = range(902, 1799)
 
 
-def read_csv(csv_file, class_whitelist=None, load_score=False):
-    """Loads boxes and class labels from a CSV file in the AVA format.
-    CSV file format described at https://research.google.com/ava/download.html.
+def load_image_lists(cfg, is_train):
+    """
+    Loading image paths from corresponding files.
+
     Args:
-      csv_file: A file object.
-      class_whitelist: If provided, boxes corresponding to (integer) class labels
-        not in this set are skipped.
+        cfg (CfgNode): config.
+        is_train (bool): if it is training dataset or not.
+
     Returns:
-      boxes: A dictionary mapping each unique image key (string) to a list of
-        boxes, given as coordinates [y1, x1, y2, x2].
-      labels: A dictionary mapping each unique image key (string) to a list of
-        integer class lables, matching the corresponding box in `boxes`.
-      scores: A dictionary mapping each unique image key (string) to a list of
-        score values lables, matching the corresponding label in `labels`. If
-        scores are not provided in the csv, then they will default to 1.0.
+        image_paths (list[list]): a list of items. Each item (also a list)
+            corresponds to one video and contains the paths of images for
+            this video.
+        video_idx_to_name (list): a list which stores video names.
     """
-    boxes = defaultdict(list)
-    labels = defaultdict(list)
-    scores = defaultdict(list)
-    with pathmgr.open(csv_file, "r") as f:
-        reader = csv.reader(f)
-        for row in reader:
-            assert len(row) in [7, 8], "Wrong number of columns: " + row
-            image_key = make_image_key(row[0], row[1])
-            x1, y1, x2, y2 = [float(n) for n in row[2:6]]
-            action_id = int(float(row[6]))
-            if class_whitelist and action_id not in class_whitelist:
-                continue
-            score = 1.0
-            if load_score:
-                score = float(row[7])
-            boxes[image_key].append([y1, x1, y2, x2])
-            labels[image_key].append(action_id)
-            scores[image_key].append(score)
-    return boxes, labels, scores
-
-
-def read_exclusions(exclusions_file):
-    """Reads a CSV file of excluded timestamps.
-    Args:
-      exclusions_file: A file object containing a csv of video-id,timestamp.
-    Returns:
-      A set of strings containing excluded image keys, e.g. "aaaaaaaaaaa,0904",
-      or an empty set if exclusions file is None.
-    """
-    excluded = set()
-    if exclusions_file:
-        with pathmgr.open(exclusions_file, "r") as f:
-            reader = csv.reader(f)
-            for row in reader:
-                assert len(row) == 2, "Expected only 2 columns, got: " + row
-                excluded.add(make_image_key(row[0], row[1]))
-    return excluded
-
-
-def read_labelmap(labelmap_file):
-    """Read label map and class ids."""
-
-    labelmap = []
-    class_ids = set()
-    name = ""
-    class_id = ""
-    with pathmgr.open(labelmap_file, "r") as f:
-        for line in f:
-            if line.startswith("  name:"):
-                name = line.split('"')[1]
-            elif line.startswith("  id:") or line.startswith("  label_id:"):
-                class_id = int(line.strip().split(" ")[-1])
-                labelmap.append({"id": class_id, "name": name})
-                class_ids.add(class_id)
-    return labelmap, class_ids
-
-
-def evaluate_ava_from_files(labelmap, groundtruth, detections, exclusions):
-    """Run AVA evaluation given annotation/prediction files."""
-
-    categories, class_whitelist = read_labelmap(labelmap)
-    excluded_keys = read_exclusions(exclusions)
-    groundtruth = read_csv(groundtruth, class_whitelist, load_score=False)
-    detections = read_csv(detections, class_whitelist, load_score=True)
-    run_evaluation(categories, groundtruth, detections, excluded_keys)
-
-
-def evaluate_ava(
-    preds,
-    original_boxes,
-    metadata,
-    excluded_keys,
-    class_whitelist,
-    categories,
-    groundtruth=None,
-    video_idx_to_name=None,
-    name="latest",
-):
-    """Run AVA evaluation given numpy arrays."""
-
-    eval_start = time.time()
-
-    detections = get_ava_eval_data(
-        preds,
-        original_boxes,
-        metadata,
-        class_whitelist,
-        video_idx_to_name=video_idx_to_name,
-    )
-
-    print("Evaluating with %d unique GT frames." % len(groundtruth[0]))
-    print(
-        "Evaluating with %d unique detection frames" % len(detections[0])
-    )
-
-    write_results(detections, "detections_%s.csv" % name)
-    write_results(groundtruth, "groundtruth_%s.csv" % name)
-
-    results = run_evaluation(categories, groundtruth, detections, excluded_keys)
-
-    print("AVA eval done in %f seconds." % (time.time() - eval_start))
-    return results["PascalBoxes_Precision/mAP@0.5IOU"]
-
-
-def run_evaluation(
-    categories, groundtruth, detections, excluded_keys, verbose=True
-):
-    """AVA evaluation main logic."""
-
-    pascal_evaluator = object_detection_evaluation.PascalDetectionEvaluator(
-        categories
-    )
-
-    boxes, labels, _ = groundtruth
-
-    gt_keys = []
-    pred_keys = []
-
-    for image_key in boxes:
-        if image_key in excluded_keys:
-            print(
-                (
-                    "Found excluded timestamp in ground truth: %s. "
-                    "It will be ignored."
-                ),
-                image_key,
-            )
-            continue
-        pascal_evaluator.add_single_ground_truth_image_info(
-            image_key,
-            {
-                standard_fields.InputDataFields.groundtruth_boxes: np.array(
-                    boxes[image_key], dtype=float
-                ),
-                standard_fields.InputDataFields.groundtruth_classes: np.array(
-                    labels[image_key], dtype=int
-                ),
-                standard_fields.InputDataFields.groundtruth_difficult: np.zeros(
-                    len(boxes[image_key]), dtype=bool
-                ),
-            },
+    list_filenames = [
+        os.path.join(cfg.AVA.FRAME_LIST_DIR, filename)
+        for filename in (
+            cfg.AVA.TRAIN_LISTS if is_train else cfg.AVA.TEST_LISTS
         )
+    ]
+    image_paths = defaultdict(list)
+    video_name_to_idx = {}
+    video_idx_to_name = []
+    for list_filename in list_filenames:
+        with pathmgr.open(list_filename, "r") as f:
+            f.readline()
+            for line in f:
+                row = line.split()
+                # The format of each row should follow:
+                # original_vido_id video_id frame_id path labels.
+                assert len(row) == 5
+                video_name = row[0]
 
-        gt_keys.append(image_key)
+                if video_name not in video_name_to_idx:
+                    idx = len(video_name_to_idx)
+                    video_name_to_idx[video_name] = idx
+                    video_idx_to_name.append(video_name)
 
-    boxes, labels, scores = detections
+                data_key = video_name_to_idx[video_name]
 
-    for image_key in boxes:
-        if image_key in excluded_keys:
-            print(
-                (
-                    "Found excluded timestamp in detections: %s. "
-                    "It will be ignored."
-                ),
-                image_key,
-            )
-            continue
-        pascal_evaluator.add_single_detected_image_info(
-            image_key,
-            {
-                standard_fields.DetectionResultFields.detection_boxes: np.array(
-                    boxes[image_key], dtype=float
-                ),
-                standard_fields.DetectionResultFields.detection_classes: np.array(
-                    labels[image_key], dtype=int
-                ),
-                standard_fields.DetectionResultFields.detection_scores: np.array(
-                    scores[image_key], dtype=float
-                ),
-            },
-        )
-
-        pred_keys.append(image_key)
-
-    metrics = pascal_evaluator.evaluate()
-
-    if du.is_master_proc():
-        pprint.pprint(metrics, indent=2)
-    return metrics
-
-
-def get_ava_eval_data(
-    scores,
-    boxes,
-    metadata,
-    class_whitelist,
-    verbose=False,
-    video_idx_to_name=None,
-):
-    """
-    Convert our data format into the data format used in official AVA
-    evaluation.
-    """
-
-    out_scores = defaultdict(list)
-    out_labels = defaultdict(list)
-    out_boxes = defaultdict(list)
-    count = 0
-    for i in range(scores.shape[0]):
-        video_idx = int(np.round(metadata[i][0]))
-        sec = int(np.round(metadata[i][1]))
-
-        video = video_idx_to_name[video_idx]
-
-        key = video + "," + "%04d" % (sec)
-        batch_box = boxes[i].tolist()
-        # The first is batch idx.
-        batch_box = [batch_box[j] for j in [0, 2, 1, 4, 3]]
-
-        one_scores = scores[i].tolist()
-        for cls_idx, score in enumerate(one_scores):
-            if cls_idx + 1 in class_whitelist:
-                out_scores[key].append(score)
-                out_labels[key].append(cls_idx + 1)
-                out_boxes[key].append(batch_box[1:])
-                count += 1
-
-    return out_boxes, out_labels, out_scores
-
-
-def write_results(detections, filename):
-    """Write prediction results into official formats."""
-    start = time.time()
-
-    boxes, labels, scores = detections
-    with pathmgr.open(filename, "w") as f:
-        for key in boxes.keys():
-            for box, label, score in zip(boxes[key], labels[key], scores[key]):
-                f.write(
-                    "%s,%.03f,%.03f,%.03f,%.03f,%d,%.04f\n"
-                    % (key, box[1], box[0], box[3], box[2], label, score)
+                image_paths[data_key].append(
+                    os.path.join(cfg.AVA.FRAME_DIR, row[3])
                 )
 
-    print("AVA results wrote to %s" % filename)
-    print("\ttook %d seconds." % (time.time() - start))
+    image_paths = [image_paths[i] for i in range(len(image_paths))]
+
+    print(
+        "Finished loading image paths from: %s" % ", ".join(list_filenames)
+    )
+
+    return image_paths, video_idx_to_name
+
+
+def load_boxes_and_labels(cfg, mode):
+    """
+    Loading boxes and labels from csv files.
+
+    Args:
+        cfg (CfgNode): config.
+        mode (str): 'train', 'val', or 'test' mode.
+    Returns:
+        all_boxes (dict): a dict which maps from `video_name` and
+            `frame_sec` to a list of `box`. Each `box` is a
+            [`box_coord`, `box_labels`] where `box_coord` is the
+            coordinates of box and 'box_labels` are the corresponding
+            labels for the box.
+    """
+    gt_lists = cfg.AVA.TRAIN_GT_BOX_LISTS if mode == "train" else []
+    pred_lists = (
+        cfg.AVA.TRAIN_PREDICT_BOX_LISTS
+        if mode == "train"
+        else cfg.AVA.TEST_PREDICT_BOX_LISTS
+    )
+    ann_filenames = [
+        os.path.join(cfg.AVA.ANNOTATION_DIR, filename)
+        for filename in gt_lists + pred_lists
+    ]
+    ann_is_gt_box = [True] * len(gt_lists) + [False] * len(pred_lists)
+
+    detect_thresh = cfg.AVA.DETECTION_SCORE_THRESH
+    # Only select frame_sec % 4 = 0 samples for validation if not
+    # set FULL_TEST_ON_VAL.
+    boxes_sample_rate = (
+        4 if mode == "val" and not cfg.AVA.FULL_TEST_ON_VAL else 1
+    )
+    all_boxes, count, unique_box_count = parse_bboxes_file(
+        ann_filenames=ann_filenames,
+        ann_is_gt_box=ann_is_gt_box,
+        detect_thresh=detect_thresh,
+        boxes_sample_rate=boxes_sample_rate,
+    )
+    print(
+        "Finished loading annotations from: %s" % ", ".join(ann_filenames)
+    )
+    print("Detection threshold: {}".format(detect_thresh))
+    print("Number of unique boxes: %d" % unique_box_count)
+    print("Number of annotations: %d" % count)
+
+    return all_boxes
+
+
+def get_keyframe_data(boxes_and_labels):
+    """
+    Getting keyframe indices, boxes and labels in the dataset.
+
+    Args:
+        boxes_and_labels (list[dict]): a list which maps from video_idx to a dict.
+            Each dict `frame_sec` to a list of boxes and corresponding labels.
+
+    Returns:
+        keyframe_indices (list): a list of indices of the keyframes.
+        keyframe_boxes_and_labels (list[list[list]]): a list of list which maps from
+            video_idx and sec_idx to a list of boxes and corresponding labels.
+    """
+
+    def sec_to_frame(sec):
+        """
+        Convert time index (in second) to frame index.
+        0: 900
+        30: 901
+        """
+        return (sec - 900) * FPS
+
+    keyframe_indices = []
+    keyframe_boxes_and_labels = []
+    count = 0
+    for video_idx in range(len(boxes_and_labels)):
+        sec_idx = 0
+        keyframe_boxes_and_labels.append([])
+        for sec in boxes_and_labels[video_idx].keys():
+            if sec not in AVA_VALID_FRAMES:
+                continue
+
+            if len(boxes_and_labels[video_idx][sec]) > 0:
+                keyframe_indices.append(
+                    (video_idx, sec_idx, sec, sec_to_frame(sec))
+                )
+                keyframe_boxes_and_labels[video_idx].append(
+                    boxes_and_labels[video_idx][sec]
+                )
+                sec_idx += 1
+                count += 1
+    print("%d keyframes used." % count)
+
+    return keyframe_indices, keyframe_boxes_and_labels
+
+
+def get_num_boxes_used(keyframe_indices, keyframe_boxes_and_labels):
+    """
+    Get total number of used boxes.
+
+    Args:
+        keyframe_indices (list): a list of indices of the keyframes.
+        keyframe_boxes_and_labels (list[list[list]]): a list of list which maps from
+            video_idx and sec_idx to a list of boxes and corresponding labels.
+
+    Returns:
+        count (int): total number of used boxes.
+    """
+
+    count = 0
+    for video_idx, sec_idx, _, _ in keyframe_indices:
+        count += len(keyframe_boxes_and_labels[video_idx][sec_idx])
+    return count
+
+
+def parse_bboxes_file(
+    ann_filenames, ann_is_gt_box, detect_thresh, boxes_sample_rate=1
+):
+    """
+    Parse AVA bounding boxes files.
+    Args:
+        ann_filenames (list of str(s)): a list of AVA bounding boxes annotation files.
+        ann_is_gt_box (list of bools): a list of boolean to indicate whether the corresponding
+            ann_file is ground-truth. `ann_is_gt_box[i]` correspond to `ann_filenames[i]`.
+        detect_thresh (float): threshold for accepting predicted boxes, range [0, 1].
+        boxes_sample_rate (int): sample rate for test bounding boxes. Get 1 every `boxes_sample_rate`.
+    """
+    all_boxes = {}
+    count = 0
+    unique_box_count = 0
+    for filename, is_gt_box in zip(ann_filenames, ann_is_gt_box):
+        with pathmgr.open(filename, "r") as f:
+            for line in f:
+                row = line.strip().split(",")
+                # When we use predicted boxes to train/eval, we need to
+                # ignore the boxes whose scores are below the threshold.
+                if not is_gt_box:
+                    score = float(row[7])
+                    if score < detect_thresh:
+                        continue
+
+                video_name, frame_sec = row[0], int(row[1])
+                if frame_sec % boxes_sample_rate != 0:
+                    continue
+
+                # Box with format [x1, y1, x2, y2] with a range of [0, 1] as float.
+                box_key = ",".join(row[2:6])
+                box = list(map(float, row[2:6]))
+                label = -1 if row[6] == "" else int(row[6])
+
+                if video_name not in all_boxes:
+                    all_boxes[video_name] = {}
+                    for sec in AVA_VALID_FRAMES:
+                        all_boxes[video_name][sec] = {}
+
+                if box_key not in all_boxes[video_name][frame_sec]:
+                    all_boxes[video_name][frame_sec][box_key] = [box, []]
+                    unique_box_count += 1
+
+                all_boxes[video_name][frame_sec][box_key][1].append(label)
+                if label != -1:
+                    count += 1
+
+    for video_name in all_boxes.keys():
+        for frame_sec in all_boxes[video_name].keys():
+            # Save in format of a list of [box_i, box_i_labels].
+            all_boxes[video_name][frame_sec] = list(
+                all_boxes[video_name][frame_sec].values()
+            )
+
+    return all_boxes, count, unique_box_count
